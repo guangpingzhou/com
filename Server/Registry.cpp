7b4aacdf38975659d48797d4724f8376f9b8f000
guangpingzhou <guangping.zhou@outlook.com>
2017-05-24 09:49:17 +0800

    com -3

--- Server/Registry.cpp
+++ Server/Registry.cpp
@@ -1,47 +1,201 @@
 // Server.cpp : 定义 DLL 应用程序的导出函数。
 //
+#include <assert.h>
 
 #include "stdafx.h"
 #include "math.h"
 
+/*
+四个重要的引出函数的实现，分别是
+DllRegisterServer,用于注册本Dll，由regsvr32.exe调用
+DllUnregisterServer，用于反注册dll，由regsver32.exe -u调用
+DllCanUnloanow，用于判断是否可以卸载本组件，由CoFreeUnusedLibraries函数调用
+DllGetClassObject，用于创建类厂并返回所需接口，由CoGetClassObject函数调用
+*/
+
+
 //Set the given key and its value
-BOOL SetKeyAndValue(const TCHAR *pszPath, const TCHAR *pszSubkey, TCHAR * pszValue);
+BOOL SetKeyAndValue(const TCHAR *szPath, const TCHAR *szSubkey, const TCHAR * szValue);
 
 //Convert a CLSID into a char string
-void CLSIDtoString(const CLSID &clsid, TCHAR *pszCLSID, int length);
+void CLSIDtoString(const CLSID &clsid, TCHAR *szCLSID, int length);
 
 //Delete szKeyChild and all of its descendents
-LONG DeleteKey(HKEY hKeyParent, const TCHAR* pszKeyString);
+LONG DeleteKey(HKEY hKeyParent, const TCHAR* szKeyString);
 
 //
 //
 //
 
 //Size of a CLSID as a string
-const int CLSID_STRING_SIZE
+const int CLSID_STRING_SIZE = 39;
 
-STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, void **ppv)
+//Register the component in the registry.
+HRESULT RegisterServer(const CLSID &clsid, 
+					const TCHAR *szFileName, 
+					const TCHAR *szProgID, 
+					const TCHAR *szDescription, 
+					const TCHAR *szVerIndProgID)
 {
-	HRESULT hr;
-	MathClassFactory * pCF;
-	if (rclsid != CLSID_Math)
-		return E_FAIL;
+	//Convert the CLSID into a char
+	TCHAR szCLSID[CLSID_STRING_SIZE];
+	CLSIDtoString(clsid, szCLSID, sizeof(szCLSID));
 
-	pCF = new MathClassFactory;
-	if (pCF == 0)
-		return E_OUTOFMEMORY;
+	//Build the key CLSID
+	TCHAR szKey[64];
+	lstrcpy(szKey, L"CLSID\\");
+	lstrcat(szKey, szCLSID);
 
-	hr = pCF->QueryInterface(riid, ppv);
-	if (FAILED(hr))
-		delete pCF;
+	//Add the server filename subkey under the CLSID key
+	SetKeyAndValue(szKey, L"InprocServer32", szFileName);
 
-	return hr;
+	//Add the ProgID subkey under the CLSID key.
+	if (szProgID != NULL)
+	{
+		SetKeyAndValue(szKey, L"ProgID", szProgID);
+		SetKeyAndValue(szKey, L"CLSID", szCLSID);
+	}
+
+	if (szVerIndProgID)
+	{
+		SetKeyAndValue(szKey, L"VersionIndepentProgID", szVerIndProgID);
+
+		// Add the version-independent ProgID subkey under HKEY_CLASSES_ROOT.
+		SetKeyAndValue(szVerIndProgID, NULL, szDescription);
+		SetKeyAndValue(szVerIndProgID, L"CLSID", szCLSID);
+		SetKeyAndValue(szVerIndProgID, L"CurVer", szProgID);
+
+		// Add the versioned ProgID subkey under HKEY_CLASSES_ROOT.
+		SetKeyAndValue(szProgID, NULL, szDescription);
+		SetKeyAndValue(szProgID, L"CLSID", szCLSID);
+	}
+
+	return S_OK;
 }
 
-STDAPI DllCanUnloadNow(void)
+//
+// Remove the component from the registry.
+//
+HRESULT UnregisterServer(const CLSID& clsid,      // Class ID
+	const TCHAR* szProgID,       //   IDs
+	const TCHAR* szVerIndProgID) // Programmatic
 {
-	if (g_lObjs || g_lLocks)
-		return(S_FALSE);
-	else
-		return(S_OK);
+	// Convert the CLSID into a char.
+	TCHAR szCLSID[CLSID_STRING_SIZE];
+	CLSIDtoString(clsid, szCLSID, sizeof(szCLSID));
+	//StringFromCLSID(clsid,szCLSID);
+	// Build the key CLSID//{...}
+	TCHAR szKey[64];
+	lstrcpy(szKey, L"CLSID//");
+	lstrcat(szKey, szCLSID);
+
+	// Delete the CLSID Key - CLSID/{...}
+	LONG lResult = DeleteKey(HKEY_CLASSES_ROOT, szKey);
+
+	// Delete the version-independent ProgID Key.
+	if (szVerIndProgID != NULL)
+		lResult = DeleteKey(HKEY_CLASSES_ROOT, szVerIndProgID);
+
+	// Delete the ProgID key.
+	if (szProgID != NULL)
+		lResult = DeleteKey(HKEY_CLASSES_ROOT, szProgID);
+
+	return S_OK;
 }
+
+// Convert a CLSID to a char string.
+void CLSIDtoString(const CLSID &clsid,
+			TCHAR *szCLSID,
+			int length)
+{
+	assert(length >= CLSID_STRING_SIZE);
+	// Get CLSID
+	LPOLESTR wszCLSID = NULL;
+	HRESULT hr = StringFromCLSID(clsid, &wszCLSID);
+	assert(SUCCEEDED(hr));
+	lstrcpy(szCLSID, wszCLSID);
+
+	// Free memory.
+	CoTaskMemFree(wszCLSID);
+}
+
+//
+// Delete a key and all of its descendents.
+//
+LONG DeleteKey(HKEY hKeyParent,           // Parent of key to delete
+			const TCHAR* lpszKeyChild)  // Key to delete
+{
+	// Open the child.
+	HKEY hKeyChild;
+	LONG lRes = RegOpenKeyEx(hKeyParent, lpszKeyChild, 0,
+		KEY_ALL_ACCESS, &hKeyChild);
+	if (lRes != ERROR_SUCCESS)
+	{
+		return lRes;
+	}
+
+	// Enumerate all of the decendents of this child.
+	FILETIME time;
+	TCHAR szBuffer[256];
+	DWORD dwSize = 256;
+	while (RegEnumKeyEx(hKeyChild, 0, szBuffer, &dwSize, NULL,
+		NULL, NULL, &time) == S_OK)
+	{
+		// Delete the decendents of this child.
+		lRes = DeleteKey(hKeyChild, szBuffer);
+		if (lRes != ERROR_SUCCESS)
+		{
+			// Cleanup before exiting.
+			RegCloseKey(hKeyChild);
+			return lRes;
+		}
+		dwSize = 256;
+	}
+
+	// Close the child.
+	RegCloseKey(hKeyChild);
+
+	// Delete this child.
+	return RegDeleteKey(hKeyParent, lpszKeyChild);
+}
+
+// Create a key and set its value.
+//
+BOOL SetKeyAndValue(const TCHAR* szKey,
+	const TCHAR* szSubkey,
+	const TCHAR* szValue)
+{
+	HKEY hKey;
+	TCHAR szKeyBuf[1024];
+
+	// Copy keyname into buffer.
+	lstrcpy(szKeyBuf, szKey);
+
+	// Add subkey name to buffer.
+	if (szSubkey != NULL)
+	{
+		lstrcat(szKeyBuf, L"//");
+		lstrcat(szKeyBuf, szSubkey);
+	}
+
+	// Create and open key and subkey.
+	long lResult = RegCreateKeyEx(HKEY_CLASSES_ROOT,
+		szKeyBuf,
+		0, NULL, REG_OPTION_NON_VOLATILE,
+		KEY_ALL_ACCESS, NULL,
+		&hKey, NULL);
+	if (lResult != ERROR_SUCCESS)	
+		return FALSE;
+
+	// Set the Value.
+	if (szValue != NULL)
+	{
+		RegSetValueEx(hKey, NULL, 0, REG_SZ,
+			(BYTE *)szValue,
+			2 * lstrlen(szValue) + 1);
+	}
+
+	RegCloseKey(hKey);
+	return TRUE;
+}
+
--- Server/dllmain.cpp
+++ Server/dllmain.cpp
@@ -2,12 +2,9 @@
 #include "stdafx.h"
 #include "math.h"
 
-//Our component category GUID
-#include "ATLDevGuide.h"
-
 //Used to store the instance handle
 //We need this to call the GetModuleFileName API
-HINSTANCE g_hinstDLL = 0;
+HINSTANCE g_hModule = NULL;
 long g_lObjs = 0;
 long g_lLocks = 0;
 
@@ -19,7 +16,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		g_hinstDLL = hModule;
+		g_hModule = hModule;
 		break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
@@ -72,111 +69,6 @@ static BOOL SetRegKeyValue(
 	}
 	return bOk;
 }
-HRESULT CreateComponentCategory(CATID catid, WCHAR* catDescription)
-{
-	ICatRegister* pcr = 0;
-	HRESULT hr;
-	hr = CoCreateInstance(CLSID_StdComponentCategoriesMgr,
-		NULL,
-		CLSCTX_INPROC_SERVER,
-		IID_ICatRegister,
-		(void**)&pcr);
-	if (FAILED(hr))
-		return hr;
-	CATEGORYINFO catinfo;
-	catinfo.catid = catid;
-	catinfo.lcid = 0x0409;
-	int len = wcslen(catDescription);
-	wcsncpy(catinfo.szDescription, catDescription, wcslen(catDescription));
-	catinfo.szDescription[len] = '\0';
-	hr = pcr->RegisterCategories(1, &catinfo);
-	pcr->Release();
-	return hr;
-}
-
-HRESULT RegisterCLSIDInCategory(REFCLSID clsid, CATID catid)
-{
-	// Register your component categories information.
-	ICatRegister* pcr = 0;
-	HRESULT hr;
-	hr = CoCreateInstance(CLSID_StdComponentCategoriesMgr,
-		NULL,
-		CLSCTX_INPROC_SERVER,
-		IID_ICatRegister,
-		(void**)&pcr);
-	if (SUCCEEDED(hr))
-	{
-		CATID rgcatid[1];
-		rgcatid[0] = catid;
-		hr = pcr->RegisterClassImplCategories(clsid, 1, rgcatid);
-		pcr->Release();
-	}
-	return hr;
-}
-
-STDAPI DllRegisterServer(void)
-{
-	HRESULT hr = NOERROR;
-	CHAR szModulePath[MAX_PATH];
-	CHAR szID[128];
-	CHAR szCLSID[128];
-	WCHAR wszID[128];
-	WCHAR wszCLSID[128];
-	GetModuleFileName(
-		g_hinstDLL,
-		szModulePath,
-		sizeof(szModulePath) / sizeof(CHAR));
-
-	StringFromGUID2(CLSID_Math, wszID, sizeof(wszID));
-	wcscpy(wszCLSID, L"CLSID\\");
-	wcscat(wszCLSID, wszID);
-	wcstombs(szID, wszID, sizeof(szID));
-	wcstombs(szCLSID, wszCLSID, sizeof(szID));
-	//Create the ProgID keys
-	SetRegKeyValue(
-		"Chapter2.Math.1",
-		NULL,
-		"Chapter2 Math Component");
-	SetRegKeyValue(
-		"Chapter2.Math.1",
-		"CLSID",
-		szID);
-	//Create version independent ProgID keys
-	SetRegKeyValue(
-		"Chapter2.Math",
-		NULL,
-		"Chapter2 Math Component");
-	SetRegKeyValue(
-		"Chapter2.Math",
-		"CurVer",
-		"Chapter2.Math.1");
-	SetRegKeyValue(
-		"Chapter2.Math",
-		"CLSID",
-		szID);
-	//Create entries under CLSID
-	SetRegKeyValue(
-		szCLSID,
-		NULL,
-		"Chapter 2 Math Component");
-	SetRegKeyValue(
-		szCLSID,
-		"ProgID",
-		"Chapter2.Math.1");
-	SetRegKeyValue(
-		szCLSID,
-		"VersionIndependentProgID",
-		"Chapter2.Math");
-	SetRegKeyValue(
-		szCLSID,
-		"InprocServer32",
-		szModulePath);
-	//Register our component category
-	CreateComponentCategory(CATID_ATLDevGuide,
-		L"ATL Developer's Guide Examples");
-	RegisterCLSIDInCategory(CLSID_Math, CATID_ATLDevGuide);
-	return S_OK;
-}
 
 STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, void ** ppv)
 {
@@ -200,4 +92,3 @@ STDAPI DllCanUnloadNow(void)
 	else
 		return(S_OK);
 }
-
